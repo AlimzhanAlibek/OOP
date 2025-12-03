773ae33cb5c7bd02803de31d62d02db0add0acbf
AlimzhanAlibek <23b55609@atu.edu.kz>
2025-12-03 16:43:38 +0500

    Delete LAB 14/Student_utilss directory

--- LAB 14/Student_utilss/Student_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .math_tools import add , average
-from .string_tools import to_upper, count_words, reverse_string
--- LAB 14/Student_utilss/Student_utils/math_tools.py
+++
@@ -1,5 +0,0 @@
-def add (a, b):
-    return a + b
-
-def average(numbers):
-    return sum(numbers) / len(numbers)
